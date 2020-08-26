3958b60de4297514f4c9a7eddb49885c1fe3f59e
unknown <kenken26031999@gmail.com>
2020-08-26 11:36:15 +0700

    first commit

---
+++ style/js/_main.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $(".dropdown-toggle").dropdown();
+});
+
+
+asNavFor: $('.carousel-main')[0]
+asNavFor: document.querySelector('.carousel-main')
+$('.carousel-main').flickity()({
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false
+});;
+// 2nd carousel, navigation
+$('.carousel-nav').flickity({
+    asNavFor: '.carousel-main',
+    contain: true,
+    prevNextButtons: false,
+    pageDots: false
+});
